80a6b684e0eb38313129a0d63047d4d2f36fdb2b
Ilya Grigorik <ilya@igvita.com>
2009-12-05 14:21:30 -0500

    (pending) test for POST requests via proxy

--- test/test_request.rb
+++ test/test_request.rb
@@ -256,20 +256,6 @@ describe EventMachine::HttpRequest do
     }
   end
 
-  it "should work with proxy servers" do
-    EventMachine.run {
-
-      http = EventMachine::HttpRequest.new('http://127.0.0.1:8080/').get :proxy => {:host => '127.0.0.1', :port => 8082}
-
-      http.errback { failed }
-      http.callback { 
-        http.response_header.status.should == 200
-        http.response.should == 'Hello, World!'
-        EventMachine.stop
-      }
-    }
-  end
-
   it "should detect deflate encoding" do
     EventMachine.run {
 
@@ -486,5 +472,40 @@ describe EventMachine::HttpRequest do
           EventMachine.stop
         }
       }
+  end   
+  
+  context "connections via proxy" do
+    
+    it "should work with proxy servers" do
+      EventMachine.run {
+
+        http = EventMachine::HttpRequest.new('http://127.0.0.1:8080/').get :proxy => {:host => '127.0.0.1', :port => 8082}
+
+        http.errback { failed }
+        http.callback { 
+          http.response_header.status.should == 200
+          http.response.should == 'Hello, World!'
+          EventMachine.stop
+        }
+      }
+    end 
+
+    it "should proxy POST data" do
+      pending
+      
+      EventMachine.run {
+
+         http = EventMachine::HttpRequest.new('http://127.0.0.1:8080/').post({
+           :body => "data", :proxy => {:host => '127.0.0.1', :port => 8082}})
+
+         http.errback { failed }
+         http.callback { 
+           http.response_header.status.should == 200
+           http.response.should match(/data/)
+           EventMachine.stop
+         }
+       }
+     end
+     
   end
 end
