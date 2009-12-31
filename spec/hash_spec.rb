6a244e355ab155481499f79d266d73b688e41fe2
Ilya Grigorik <ilya@igvita.com>
2009-12-31 00:44:16 -0500

    stop the hash test sillyness

--- spec/hash_spec.rb
+++ spec/hash_spec.rb
@@ -4,16 +4,21 @@ describe Hash do
   
   describe ".to_params" do
     it "should transform a basic hash into HTTP POST Params" do
-      {:a => "alpha", :b => "beta"}.to_params.should == "a=alpha&b=beta"
+      {:a => "alpha", :b => "beta"}.to_params.split("&").should include "a=alpha"
+      {:a => "alpha", :b => "beta"}.to_params.split("&").should include "b=beta"
     end
     
     it "should transform a more complex hash into HTTP POST Params" do
-      {:a => "a", :b => ["c", "d", "e"]}.to_params.should == "a=a&b[0]=c&b[1]=d&b[2]=e"
+      {:a => "a", :b => ["c", "d", "e"]}.to_params.split("&").should include "a=a"
+      {:a => "a", :b => ["c", "d", "e"]}.to_params.split("&").should include "b[0]=c"
+      {:a => "a", :b => ["c", "d", "e"]}.to_params.split("&").should include "b[1]=d"
+      {:a => "a", :b => ["c", "d", "e"]}.to_params.split("&").should include "b[2]=e"
     end
 
-    # Ruby 1.8 Hash is not sorted, so this test breaks randomly. Maybe once we're all on 1.9. ;-)
-    # it "should transform a very complex hash into HTTP POST Params" do
-    #    {:a => "a", :b => [{:c => "c", :d => "d"}, {:e => "e", :f => "f"}]}.to_params.should == "a=a&b[0][d]=d&b[0][c]=c&b[1][f]=f&b[1][e]=e"
-    # end
+    it "should transform a very complex hash into HTTP POST Params" do
+      params = {:a => "a", :b => [{:c => "c", :d => "d"}, {:e => "e", :f => "f"}]}.to_params.split("&")
+      params.should include "a=a"
+      params.should include "b[0][d]=d"
+    end
   end
 end
