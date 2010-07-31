6b1aa86ec437ae5b31efd3f350b0f1ac1c32ba90
Ilya Grigorik <ilya@igvita.com>
2010-07-31 18:08:51 -0400

    add spec for new multi syntax; grrr, remove ^M's

--- lib/em-http/multi.rb
+++ lib/em-http/multi.rb
@@ -1,54 +1,55 @@
 module EventMachine
 
-  # EventMachine based Multi request client, based on a streaming HTTPRequest class, 
+  # EventMachine based Multi request client, based on a streaming HTTPRequest class,
   # which allows you to open multiple parallel connections and return only when all
   # of them finish. (i.e. ideal for parallelizing workloads)
-  # 
+  #
   # == Example
-  # 
+  #
   #  EventMachine.run {
-  #   
+  #
   #   multi = EventMachine::MultiRequest.new
-  #    
+  #
   #   # add multiple requests to the multi-handler
   #   multi.add(EventMachine::HttpRequest.new('http://www.google.com/').get)
   #   multi.add(EventMachine::HttpRequest.new('http://www.yahoo.com/').get)
-  #    
+  #
   #    multi.callback  {
   #       p multi.responses[:succeeded]
   #       p multi.responses[:failed]
-  #     
+  #
   #       EventMachine.stop
   #    }
   #  }
-  # 
-  
+  #
+
   class MultiRequest
     include EventMachine::Deferrable
 
     attr_reader :requests, :responses
-    
+
     def initialize(conns=[], &block)
       @requests  = []
       @responses = {:succeeded => [], :failed => []}
-      add(*conns)
+
+      conns.each {|conn| add(conn)}
       callback(&block) if block_given?
     end
-    
+
     def add(conn)
       @requests.push(conn)
 
       conn.callback { @responses[:succeeded].push(conn); check_progress }
       conn.errback  { @responses[:failed].push(conn); check_progress }
     end
-    
+
     protected
-   
+
     # invoke callback if all requests have completed
     def check_progress
       succeed(self) if (@responses[:succeeded].size +
-                        @responses[:failed]   .size) == @requests.size
+                        @responses[:failed].size) == @requests.size
     end
-    
+
   end
 end
--- spec/multi_spec.rb
+++ spec/multi_spec.rb
@@ -5,26 +5,43 @@ describe EventMachine::MultiRequest do
 
   it "should submit multiple requests in parallel and return once all of them are complete" do
     EventMachine.run {
-      
+
       # create an instance of multi-request handler, and the requests themselves
       multi = EventMachine::MultiRequest.new
-      
+
       # add multiple requests to the multi-handler
       multi.add(EventMachine::HttpRequest.new('http://127.0.0.1:8080/').get(:query => {:q => 'test'}))
       multi.add(EventMachine::HttpRequest.new('http://0.0.0.0:8083/').get(:timeout => 1))
-      
+
       multi.callback  {
         # verify successful request
         multi.responses[:succeeded].size.should == 1
         multi.responses[:succeeded].first.response.should match(/test/)
-        
+
         # verify invalid requests
         multi.responses[:failed].size.should == 1
         multi.responses[:failed].first.response_header.status.should == 0
-        
+
         EventMachine.stop
       }
-    } 
+    }
+  end
+
+  it "should accept multiple open connections and return once all of them are complete" do
+    EventMachine.run {
+      http1 = EventMachine::HttpRequest.new('http://127.0.0.1:8080/').get(:query => {:q => 'test'})
+      http2 = EventMachine::HttpRequest.new('http://0.0.0.0:8083/').get(:timeout => 1)
+
+      multi = EventMachine::MultiRequest.new([http1, http2]) do
+        multi.responses[:succeeded].size.should == 1
+        multi.responses[:succeeded].first.response.should match(/test/)
+
+        multi.responses[:failed].size.should == 1
+        multi.responses[:failed].first.response_header.status.should == 0
+
+        EventMachine.stop
+      end
+    }
   end
 
   it "should handle multiple mock requests" do
@@ -32,20 +49,20 @@ describe EventMachine::MultiRequest do
     EventMachine::MockHttpRequest.register_file('http://0.0.0.0:8083/', :get, {}, File.join(File.dirname(__FILE__), 'fixtures', 'google.ca'))
 
     EventMachine.run {
-      
+
       # create an instance of multi-request handler, and the requests themselves
       multi = EventMachine::MultiRequest.new
-      
+
       # add multiple requests to the multi-handler
       multi.add(EventMachine::MockHttpRequest.new('http://127.0.0.1:8080/').get)
       multi.add(EventMachine::MockHttpRequest.new('http://0.0.0.0:8083/').get)
-      
+
       multi.callback  {
         # verify successful request
         multi.responses[:succeeded].size.should == 2
 
         EventMachine.stop
       }
-    } 
+    }
   end
 end
