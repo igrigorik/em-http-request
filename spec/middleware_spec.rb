66387535f4c3867dbbd2c6125a5ca1a95b16cd2b
Ilya Grigorik <ilya@igvita.com>
2011-02-01 12:32:12 -0500

    request middleware + example

--- spec/middleware_spec.rb
+++ spec/middleware_spec.rb
@@ -2,16 +2,6 @@ require 'helper'
 
 describe EventMachine::HttpRequest do
 
-  class TestMiddleware
-    def self.request
-    end
-
-    def self.response(resp)
-      resp.response_header['X-Header'] = 'middleware'
-      resp.response = 'Hello, Middleware!'
-    end
-  end
-
   module EmptyMiddleware; end
 
   class GlobalMiddleware
@@ -23,8 +13,8 @@ describe EventMachine::HttpRequest do
   it "should accept middleware" do
     EventMachine.run {
       lambda {
-        conn = EM::HttpRequest.new('http://127.0.0.1:8090') 
-        conn.use TestMiddleware
+        conn = EM::HttpRequest.new('http://127.0.0.1:8090')
+        conn.use ResponseMiddleware
         conn.use EmptyMiddleware
 
         EM.stop
@@ -32,32 +22,95 @@ describe EventMachine::HttpRequest do
     }
   end
 
-  it "should execute response middleware before user callbacks" do
-   EventMachine.run {
-      conn = EM::HttpRequest.new('http://127.0.0.1:8090') 
-      conn.use TestMiddleware
+  context "request" do
+    class ResponseMiddleware
+      def self.response(resp)
+        resp.response_header['X-Header'] = 'middleware'
+        resp.response = 'Hello, Middleware!'
+      end
+    end
 
-      req = conn.get
-      req.callback {
-        req.response_header['X-Header'].should match('middleware')
-        req.response.should match('Hello, Middleware!')
-        EM.stop
+
+    it "should execute response middleware before user callbacks" do
+      EventMachine.run {
+        conn = EM::HttpRequest.new('http://127.0.0.1:8090')
+        conn.use ResponseMiddleware
+
+        req = conn.get
+        req.callback {
+          req.response_header['X-Header'].should match('middleware')
+          req.response.should match('Hello, Middleware!')
+          EM.stop
+        }
       }
-    }
+    end
+
+    it "should execute global response middleware before user callbacks" do
+      EventMachine.run {
+        EM::HttpRequest.use GlobalMiddleware
+
+        conn = EM::HttpRequest.new('http://127.0.0.1:8090')
+
+        req = conn.get
+        req.callback {
+          req.response_header['X-Global'].should match('middleware')
+          EM.stop
+        }
+      }
+    end
   end
 
-  it "should execute global response middleware before user callbacks" do
-   EventMachine.run {
-      EM::HttpRequest.use GlobalMiddleware
+  context "request" do
+    class RequestMiddleware
+      def self.request(head, body)
+        head['X-Middleware'] = 'middleware'   # insert new header
+        body += ' modified'                   # modify post body
 
-      conn = EM::HttpRequest.new('http://127.0.0.1:8090')
+        [head, body]
+      end
+    end
 
-      req = conn.get
-      req.callback {
-        req.response_header['X-Global'].should match('middleware')
-        EM.stop
+    it "should execute request middleware before dispatching request" do
+      EventMachine.run {
+        conn = EventMachine::HttpRequest.new('http://127.0.0.1:8090/')
+        conn.use RequestMiddleware
+
+        req = conn.post :body => "data"
+        req.callback {
+          req.response_header.status.should == 200
+          req.response.should match(/data modified/)
+          EventMachine.stop
+        }
       }
-    }
+    end
+  end
+
+  context "jsonify" do
+    class JSONify
+      require 'json'
+
+      def self.request(head, body)
+        [head, JSON.generate(body)]
+      end
+
+      def self.response(resp)
+        resp.response = JSON.parse(resp.response)
+      end
+    end
+
+    it "should use middleware to JSON encode and JSON decode the body" do
+      EventMachine.run {
+        conn = EventMachine::HttpRequest.new('http://127.0.0.1:8090/')
+        conn.use JSONify
+
+        req = conn.post :body => {:ruby => :hash}
+        req.callback {
+          req.response_header.status.should == 200
+          req.response.should == {"ruby" => "hash"}
+          EventMachine.stop
+        }
+      }
+    end
   end
 
 end
