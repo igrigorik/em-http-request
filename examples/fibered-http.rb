754b3e9a939ea941ec0d8fa21848512f21122737
Ilya Grigorik <ilya@igvita.com>
2011-04-16 18:58:28 -0400

    don't suspen fiber on bad DNS in example - closes #95

--- examples/fibered-http.rb
+++ examples/fibered-http.rb
@@ -9,17 +9,21 @@ def async_fetch(url)
   f = Fiber.current
   http = EventMachine::HttpRequest.new(url).get :timeout => 10
 
-  http.callback { f.resume(http) }
-  http.errback  { f.resume(http) }
+  if http.error.empty?
+    http.callback { f.resume(http) }
+    http.errback  { f.resume(http) }
 
-  return Fiber.yield
+    Fiber.yield
+  else
+    http
+  end
 end
 
 EventMachine.run do
   Fiber.new{
 
     puts "Setting up HTTP request #1"
-    data = async_fetch('http://www.google.com/')
+    data = async_fetch('http://www.google.moo/')
     puts "Fetched page #1: #{data.response_header.status}"
 
     puts "Setting up HTTP request #2"
