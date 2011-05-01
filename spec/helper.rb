483d921d3172c5dd1421dad8927876e17f258e00
Ilya Grigorik <ilya@igvita.com>
2011-05-01 14:32:04 -0400

    run socks5 spec if the local tunnel is open

--- spec/helper.rb
+++ spec/helper.rb
@@ -15,3 +15,15 @@ end
 def requires_connection(&blk)
   blk.call if system('ping -t1 -c1 google.com &> /dev/null')
 end
+
+def requires_port(port, &blk)
+  port_open = true
+  begin
+    s = TCPSocket.new('localhost', port)
+    s.close()
+  rescue
+    port_open = false
+  end
+
+  blk.call if port_open
+end
--- spec/socksify_proxy_spec.rb
+++ spec/socksify_proxy_spec.rb
@@ -1,24 +1,24 @@
 require 'helper'
 
 requires_connection do
+  requires_port(8080) do
+    describe EventMachine::HttpRequest do
 
-  describe EventMachine::HttpRequest do
+      # ssh -D 8080 igvita
+      let(:proxy) { {:proxy => { :host => '127.0.0.1', :port => 8080, :type => :socks5 }} }
 
-    # ssh -D 8080 igvita
-    let(:proxy) { {:proxy => { :host => '127.0.0.1', :port => 8080, :type => :socks5 }} }
+      it "should use SOCKS5 proxy" do
+        EventMachine.run {
+          http = EventMachine::HttpRequest.new('http://jsonip.com/', proxy).get
 
-    it "should use SOCKS5 proxy" do
-      EventMachine.run {
-        http = EventMachine::HttpRequest.new('http://jsonip.com/', proxy).get
-
-        http.errback { failed(http) }
-        http.callback {
-          http.response_header.status.should == 200
-          http.response.should match('72.52.131')
-          EventMachine.stop
+          http.errback { failed(http) }
+          http.callback {
+            http.response_header.status.should == 200
+            http.response.should match('72.52.131')
+            EventMachine.stop
+          }
         }
-      }
+      end
     end
   end
-
 end
