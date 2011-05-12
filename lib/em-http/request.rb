17b264c3a4b9b8e302c967061fcfa8d59b3a6dcd
John W Higgins <wishdev@gmail.com>
2011-05-11 19:53:09 -0700

    Defer connection until needed

--- lib/em-http/request.rb
+++ lib/em-http/request.rb
@@ -3,35 +3,12 @@ module EventMachine
     @middleware = []
 
     def self.new(uri, options={})
-      begin
-        connopt = HttpConnectionOptions.new(uri, options)
+      connopt = HttpConnectionOptions.new(uri, options)
 
-        EventMachine.connect(connopt.host, connopt.port, HttpConnection) do |c|
-          c.connopts = connopt
-          c.uri = uri
-
-          c.pending_connect_timeout = connopt.connect_timeout
-          c.comm_inactivity_timeout = connopt.inactivity_timeout
-        end
-
-      rescue EventMachine::ConnectionError => e
-        #
-        # Currently, this can only fire on initial connection setup
-        # since #connect is a synchronous method. Hence, rescue the
-        # exception, and return a failed deferred which will immediately
-        # fail any client request.
-        #
-        # Once there is async-DNS, then we'll iterate over the outstanding
-        # client requests and fail them in order.
-        #
-        # Net outcome: failed connection will invoke the same ConnectionError
-        # message on the connection deferred, and on the client deferred.
-        #
-        conn = EventMachine::FailedConnection.new(uri, connopt)
-        conn.error = e.message
-        conn.fail
-        conn
-      end
+      c = HttpConnection.new
+      c.connopts = connopt
+      c.uri = uri
+      c
     end
 
     def self.use(klass, *args, &block)
