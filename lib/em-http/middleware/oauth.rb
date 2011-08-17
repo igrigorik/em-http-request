de1294eab6bb8e7e7792b219c4b2ff0f6edceb76
Andrew Kane <andrew@getformidable.com>
2011-08-17 00:03:08 -0700

    Allow both `oauth` gem and `simple_oauth` gem opts format

--- lib/em-http/middleware/oauth.rb
+++ lib/em-http/middleware/oauth.rb
@@ -5,7 +5,10 @@ module EventMachine
 
     class OAuth
       def initialize(opts = {})
-        @opts = opts
+        @opts = opts.dup
+        # Allow both `oauth` gem and `simple_oauth` gem opts formats
+        @opts[:token] ||= @opts.delete(:access_token)
+        @opts[:token_secret] ||= @opts.delete(:access_token_secret)
       end
 
       def request(client, head, body)
