53dd586639dfccd178bdac440a8fc153d23eae70
Andrew Kane <andrew@getformidable.com>
2011-08-23 00:55:38 -0700

    Fixed OAuth issue when `:query` and `:body` are strings

--- lib/em-http/middleware/oauth.rb
+++ lib/em-http/middleware/oauth.rb
@@ -4,6 +4,8 @@ module EventMachine
   module Middleware
 
     class OAuth
+      include HttpEncoding
+
       def initialize(opts = {})
         @opts = opts.dup
         # Allow both `oauth` gem and `simple_oauth` gem opts formats
@@ -14,9 +16,17 @@ module EventMachine
       def request(client, head, body)
         request = client.req
 
-        params = request.query.to_a + body.to_a
+        uri = request.uri.join(encode_query(request.uri, request.query))
 
-        head["Authorization"] = SimpleOAuth::Header.new(request.method, request.uri, params, @opts)
+        params = {}
+        if ["POST", "PUT"].include?(request.method)
+          CGI.parse(client.normalize_body(body)).each do |k,v|
+            # Since `CGI.parse` always returns values as an array
+            params[k] = v.size == 1 ? v.first : v
+          end
+        end
+
+        head["Authorization"] = SimpleOAuth::Header.new(request.method, uri, params, @opts)
 
         [head,body]
       end
