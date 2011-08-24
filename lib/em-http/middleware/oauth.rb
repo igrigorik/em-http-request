76ee916d2be5ba15a09f6af177fef7637fb242e2
Andrew Kane <andrew@getformidable.com>
2011-08-23 23:12:05 -0700

    Check for form encoded data in OAuth middleware

--- lib/em-http/middleware/oauth.rb
+++ lib/em-http/middleware/oauth.rb
@@ -15,14 +15,19 @@ module EventMachine
 
       def request(client, head, body)
         request = client.req
+        uri     = request.uri.join(encode_query(request.uri, request.query))
+        params  = {}
 
-        uri = request.uri.join(encode_query(request.uri, request.query))
-
-        params = {}
+        # from https://github.com/oauth/oauth-ruby/blob/master/lib/oauth/request_proxy/em_http_request.rb
         if ["POST", "PUT"].include?(request.method)
-          CGI.parse(client.normalize_body(body)).each do |k,v|
-            # Since `CGI.parse` always returns values as an array
-            params[k] = v.size == 1 ? v.first : v
+          head["content-type"] ||= "application/x-www-form-urlencoded" if body.is_a? Hash
+          form_encoded = head["content-type"].to_s.downcase.start_with?("application/x-www-form-urlencoded")
+
+          if form_encoded
+            CGI.parse(client.normalize_body(body)).each do |k,v|
+              # Since `CGI.parse` always returns values as an array
+              params[k] = v.size == 1 ? v.first : v
+            end
           end
         end
 
