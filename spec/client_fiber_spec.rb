ce50f322ce08d43a4a747cf333ea576765d764c4
Ilya Grigorik <ilya@igvita.com>
2012-01-07 10:28:17 -0800

    add a check in fiber spec

--- spec/client_fiber_spec.rb
+++ spec/client_fiber_spec.rb
@@ -3,19 +3,21 @@ require 'fiber'
 
 describe EventMachine::HttpRequest do
   context "with fibers" do
-    it "should be transparent to connexion errors" do
+
+    it "should be transparent to connection errors" do
       EventMachine.run do
         Fiber.new do
           f = Fiber.current
-          http = EventMachine::HttpRequest.new('http://non-existing.domain/').get
-          http.callback {failed(http)}
-          http.errback {f.resume}
-          Fiber.yield
-          EventMachine.stop
+          fired = false
+          http = EventMachine::HttpRequest.new('http://non-existing.domain/', :connection_timeout => 0.1).get
+          http.callback { failed(http) }
+          http.errback { f.resume :errback }
+
+          Fiber.yield.should == :errback
+          EM.stop
         end.resume
       end
     end
+
   end
 end
-
-
