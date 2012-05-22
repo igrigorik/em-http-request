96e296a445536309e39a25e44c85c3fd6d437880
Ezekiel Templin <zeke@templ.in>
2012-05-22 03:44:54 -0400

    Authenticated HTTP proxy patch

--- lib/em-http/client.rb
+++ lib/em-http/client.rb
@@ -133,9 +133,9 @@ module EventMachine
 
     def build_request
       head    = @req.headers ? munge_header_keys(@req.headers) : {}
-
-      if @req.http_proxy?
-        head['proxy-authorization'] = @req.proxy[:authorization] if @req.proxy[:authorization]
+      
+      if proxy = @conn.connopts.proxy
+        head['proxy-authorization'] = proxy[:authorization] if proxy[:authorization]
       end
 
       # Set the cookie header if provided
