f413010bc3313ed6cb8443abe1754d9a884127a4
Joel Jackson <jackson.joel@gmail.com>
2013-05-11 02:11:00 -0700

    fix for error when promise library is missing as well as calling the failure handler when the http status is not 200

--- lib/em-http/promising_client.rb
+++ lib/em-http/promising_client.rb
@@ -1,10 +1,15 @@
-if require 'em-promise' || defined?(EM::Q)
+begin                                                                                                                                                                                                      
+  require 'em-promise'                                                                                                                                                                                     
   module PromisingHttpRequest                                                                                                                                                                              
     def then(success_back = nil, error_back = nil, &blk)                                                                                                                                                   
       deferred = EM::Q.defer                                                                                                                                                                               
                                                                                                                                                                                                            
       self.callback {                                                                                                                                                                                      
-        deferred.resolve(self)                                                                                                                                                                             
+        if self.response_header.status == 200                                                                                                                                                              
+          deferred.resolve(self)                                                                                                                                                                           
+        else                                                                                                                                                                                               
+          deferred.reject(self)                                                                                                                                                                            
+        end                                                                                                                                                                                                
       }                                                                                                                                                                                                    
       self.errback {                                                                                                                                                                                       
         deferred.reject("Connection failure")                                                                                                                                                              
@@ -13,9 +18,10 @@ if require 'em-promise' || defined?(EM::Q)
     end                                                                                                                                                                                                    
                                                                                                                                                                                                            
     def is_a?(klass)                                                                                                                                                                                       
-      return true if klass == EventMachine::Q::Promise                                                                                                                                                     
+      return true if klass == EventMachine::Q::Promise && ! self.finished?                                                                                                                                 
       super(klass)                                                                                                                                                                                         
     end                                                                                                                                                                                                    
   end                                                                                                                                                                                                      
-  EM::HttpClient.send(:include, PromisingHttpRequest) 
-end
+  EM::HttpClient.send(:include, PromisingHttpRequest)                                                                                                                                                      
+rescue LoadError => e                                                                                                                                                                                      
+end   
