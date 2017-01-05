f71313a8a288f91d43d464695ea484b2cbe8cb6a
mexisme <wildjim+dev@kiwinet.org>
2017-01-05 17:06:51 +1300

    Support clearing the *_PROXY env-vars during test runs
    FIXES #306

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,25 @@
+PROXY_ENV_VARS = %w[HTTP_PROXY http_proxy HTTPS_PROXY https_proxy ALL_PROXY]
+
 RSpec.configure do |config|
+  proxy_envs = {}
+
   config.mock_with :rspec do |c|
     c.syntax = [:should, :expect]
   end
   config.expect_with :rspec do |c|
     c.syntax = [:should, :expect]
+  end
+
+  config.before :all do
+    # Back-up ENV *_PROXY vars
+    orig_proxy_envs = Hash[
+      PROXY_ENV_VARS.select {|k| ENV.key? k }.map {|k| [k, ENV.delete(k)] }
+    ]
+    proxy_envs.replace(orig_proxy_envs)
+  end
+
+  config.after :all do
+    # Restore ENV *_PROXY vars
+    ENV.update(proxy_envs)
   end
 end
